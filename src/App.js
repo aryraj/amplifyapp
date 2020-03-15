1ede9858c261a1569b79ac738bc0a534617b1373
ary3003 <aryan5416@gmail.com>
2020-03-15 17:51:38 +0530

    test change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React deploy on AWS
         </a>
       </header>
     </div>
